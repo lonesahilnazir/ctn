76c88350c5835db06d993be4c688cf9ea435fc33
SAHIL-NAZIR <sahilnazirlone26@gmail.com>
2022-01-04 12:17:07 +0530

    Map component completed.

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
+import './map.css'
+
+import map from '../../assets/map.png'
+
 const Map = () => {
     return (
-        <div>
-            Map
+        <div className='map'>
+            <img src={map} alt="" />
         </div>
     )
 }
