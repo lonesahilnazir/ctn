d698deda440a74b22f4a249f58e21f3b99425779
SAHIL-NAZIR <sahilnazirlone26@gmail.com>
2022-01-04 18:00:24 +0530

    Article component completed.

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
-const Article = () => {
+import './article.css'
+
+import { Button } from '../../components'
+
+const Article = ({button, title, text}) => {
     return (
-        <div>
-            Article
+        <div className='article'>
+            <h3>{title}</h3>
+            <p>{text}</p>
+            {button && <Button text="Read More" class="btn btn--hollow" />}
         </div>
     )
 }
