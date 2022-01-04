206413d8cc8e2492980aca7911a6b656d60737de
SAHIL-NAZIR <sahilnazirlone26@gmail.com>
2022-01-04 19:41:45 +0530

    Blogs container completed.

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ const App = () => {
             <OurTeam />
             <Message />
             <ContactUs />
+            <Blogs />
         </div>
     )
 }
--- src/containers/blogs/Blogs.jsx
+++ src/containers/blogs/Blogs.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Article } from '../../components'
+
+import './blogs.css'
 
 const Blogs = () => {
     return (
-        <div>
-            Blogs
+        <div className='blogs'>
+            <div className='blogs__heading'>
+                <h2>Latest Blogs</h2>
+                <p>Blessing welcomed ladyship she met humoured sir breeding her. Six curiosity day assurance bed necessary.</p>
+            </div>
+            <div className='blogs__articles'>
+                <Article button={false} title='Unleash Your Creativity' text='Nine for Mortal Men, doomed to die, One for' />
+                <Article button={true} title='Unleash Your Creativity' text='Nine for Mortal Men, doomed to die, One for' />
+                <Article button={true} title='Unleash Your Creativity' text='Nine for Mortal Men, doomed to die, One for' />
+                <Article button={false} title='Unleash Your Creativity' text='Nine for Mortal Men, doomed to die, One for' />
+            </div>
         </div>
     )
 }
