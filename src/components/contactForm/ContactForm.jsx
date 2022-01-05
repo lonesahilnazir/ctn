c8fb8f311056e5da5d7da6fdc61ce229696f755b
SAHIL-NAZIR <sahilnazirlone26@gmail.com>
2022-01-05 11:43:17 +0530

    bug fix.

--- src/components/contactForm/ContactForm.jsx
+++ src/components/contactForm/ContactForm.jsx
@@ -10,19 +10,19 @@ const ContactForm = () => {
             <form className='form'>
                 <div className='form__name'>
                     <input className='form__input' type="text" id="name" name="name" placeholder=" " />
-                    <label className='form__label' For="name">Name</label>
+                    <label className='form__label' htmlFor="name">Name</label>
                 </div>
                 <div className='form__email'>
                     <input className='form__input' type="email" id="email" name="email" placeholder=" " />
-                    <label className='form__label' For="email">Email</label>
+                    <label className='form__label' htmlFor="email">Email</label>
                 </div>
                 <div className='form__subject'>
                     <input className='form__input' type="text" id="subject" name="subject" placeholder=" " />
-                    <label className='form__label' For="subject">Subject</label>
+                    <label className='form__label' htmlFor="subject">Subject</label>
                 </div>
                 <div className='form__message'>
                     <input className='form__input' id="message" name="message" placeholder=" " />
-                    <label className='form__label' For="message">Message</label>
+                    <label className='form__label' htmlFor="message">Message</label>
                 </div>
                 <Button text="Send Message" class="btn btn--full" />
             </form>
