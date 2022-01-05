1f017a6ef5168dd650e83d1e3ef5f4b4f5cdd64b
SAHIL-NAZIR <sahilnazirlone26@gmail.com>
2022-01-05 15:43:16 +0530

    Responsive Navbar.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,17 +3,27 @@ import React from 'react';
 import './navbar.css';
 
 const Navbar = () => {
+    const [isOpen, setisOpen] = React.useState(false)
+    function handleClick() {
+        setisOpen(!isOpen);
+      }
+
     return (
         <div className='navbar' id='nav'>
             <div className='navbar__logo'>
                 <h1>CONSTRUCTION</h1>
             </div>
-            <div className='navbar__links'>
+            <div className={`navbar__links ${ isOpen ? "navbar__links--block" : "navbar__links--hidden" }`}>
                 <a href='#nav'>Home</a>
                 <a href='#'>About Us</a>
                 <a href='#'>Services</a>
                 <a href='#'>Project</a>
             </div>
+            <button className={`navbar__toggle-menu ${ isOpen && "navbar__toggle-menu--open"}`} id='menu-bar' onClick={handleClick}>
+                <span />
+                <span />
+                <span />
+            </button>
         </div>
     )
 }
